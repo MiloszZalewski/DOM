06c8669adf05384625d97e71ec64d69be51347d7
Miłosz Zalewski <zalewski.milosz@gmail.com>
2017-12-13 20:08:45 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 var withButtonClass = document.getElementsByClassName('button');
-
+var buttonLength = buttonClass.length;
 console.log(withButtonClass);
 
-for (var i = 0; i <= withButtonClass.length; i++ ) {
-	console.log(document.withButtonClass.innerText);
+for (var i = 0; i <= buttonLength; i++ ) {
+	console.log(document.withButtonClass[i].innerText);
 }
